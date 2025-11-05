2d2413a4f8a31e7a3fe8cc23da98c1326461b204
FlorianZaczyk <zaczyk.florian@gmail.com>
2025-11-05 22:44:07 +0100

    Hausaufgabe Update
    statt auf SpriteRenderer bezieht sich die Drehung des Charakters auf die Rotation von der Transform-Komponente
    + andere fehlerhafte PlayerController gelöscht

--- Assets/+++Workdata/Scenes/Player/PlayerController.cs
+++ Assets/+++Workdata/Scenes/Player/PlayerController.cs
@@ -1,46 +1,24 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerControllerAA : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public static readonly int Hash_MovementValues = Animator.StringToHash("MovementValues");
-    public static readonly int Hash_GroundValue = Animator.StringToHash("isGrounded");
-
-    
     #region Insepctor Variables 
 // Alle Variabeln die im Inspektor zu sehen sind. 
-    [Header("Movement")]
     [SerializeField] private float _walkingSpeed =5f;
     [SerializeField] private float _runningSpeed = 7f;
-    [SerializeField] private float _jumpPower = 3f;
 
-
-    [Header("GroundCheck")]
-    [SerializeField] private Vector2 boxSize;
-    [SerializeField] private Vector2 boxOffset;
-    [SerializeField] private LayerMask _groundLayer;
-    
-    
     #endregion
 
     #region Private Variables
-    private Vector2 _moveInput;
     
+    private Vector2 _moveInput;
+    private Transform _ts;
     private Rigidbody2D _rb;
-
-    private float _moveSpeed = 5f;
     private SpriteRenderer _sr;
-    private Animator _anim;
-
+    private float _moveSpeed = 5f;
     private bool isFacingRight = true;
-    private bool isGrounded;
-    private bool isJumping;
-    private bool isRolling;
-    private bool isAttacking;
-    private bool canJump;
     
     #endregion
     
@@ -69,7 +47,8 @@ public class PlayerControllerAA : MonoBehaviour
     {
         _rb = gameObject.GetComponent<Rigidbody2D>();
         _sr = GetComponent<SpriteRenderer>();
-        _anim =  GetComponent<Animator>();
+        _ts = GetComponent<Transform>();
+        
         
          _moveSpeed = _walkingSpeed;
          
@@ -94,73 +73,35 @@ public class PlayerControllerAA : MonoBehaviour
 
     private void FixedUpdate()
     {
-        CheckGround();
-        
         _rb.linearVelocityX = _moveInput.x * _walkingSpeed;
-
-        UpdateAnimator();
     }
 
     private void OnDisable()
     {
         _inputActions.Disable();
         _moveAction.performed -= Move;
-        _moveAction.canceled -= Move;
-        
-    }
-    #endregion
-    
-    #region Physics
+        _moveAction.canceled -= Move; 
 
-    void CheckGround()
-    {
-        isGrounded = Physics2D.OverlapBox(boxOffset + (Vector2)transform.position + boxSize, 0, _groundLayer);
     }
-
-    #endregion
-        
-    #region Animations Methods
-    void UpdateAnimator()
-    {
-        _anim.SetFloat(Hash_MovementValues, Mathf.Abs(_rb.linearVelocity.x));
-        _anim.SetBool(Hash_GroundValue, isGrounded);
-    }
-        
-    #endregion
-
 
     #endregion
 
     #region Input
-    
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = isGrounded ? Color.green : Color.red;
-        
-        Gizmos.DrawWireCube(boxOffset + (Vector2)transform.position + boxSize);
-    }
     private void Move(InputAction.CallbackContext ctx)
     {
         _moveInput = ctx.ReadValue<Vector2>();
-        _sr = GetComponent<SpriteRenderer>();
         
         if (_moveInput.x > 0)
-        {
-            _sr.flipX = false;
-            isFacingRight = true;
+        { 
+            _ts.rotation = Quaternion.Euler(0, 0, 0);
         }
         else if (_moveInput.x < 0)
-        {
-            _sr.flipX = true;
-            isFacingRight = false;
+        { 
+            _ts.rotation = Quaternion.Euler(0, 180, 0);
         }
         
-        
     }
-
+    
     #endregion
-    
-    
-    
-   
+  
 }
