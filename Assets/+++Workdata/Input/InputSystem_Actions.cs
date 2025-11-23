23dfd218b2afac3b0e16dda5ec4fa150e0eeb53c
FlorianZaczyk <zaczyk.florian@gmail.com>
2025-11-23 20:55:21 +0100

    23.11.2025 player Attack combo HA
    zweite attacke hinzugefügt und leichte combo

--- Assets/+++Workdata/Input/InputSystem_Actions.cs
+++ Assets/+++Workdata/Input/InputSystem_Actions.cs
@@ -114,7 +114,16 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""name"": ""Attack"",
                     ""type"": ""Button"",
                     ""id"": ""6c2ab1b8-8984-453a-af3d-a3c78ae1679a"",
-                    ""expectedControlType"": ""Button"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""SecondAttack"",
+                    ""type"": ""Button"",
+                    ""id"": ""95eae018-9afd-494d-ac7d-f14c0af19d54"",
+                    ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
@@ -123,7 +132,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""name"": ""Interact"",
                     ""type"": ""Button"",
                     ""id"": ""852140f2-7766-474d-8707-702459ba45f3"",
-                    ""expectedControlType"": ""Button"",
+                    ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": ""Hold"",
                     ""initialStateCheck"": false
@@ -588,6 +597,17 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""Roll"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""cb80478d-3445-422b-9ef4-1405a5135fe5"",
+                    ""path"": ""<Mouse>/rightButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": "";Keyboard&Mouse"",
+                    ""action"": ""SecondAttack"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -1145,6 +1165,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
         m_Player_Attack = m_Player.FindAction("Attack", throwIfNotFound: true);
+        m_Player_SecondAttack = m_Player.FindAction("SecondAttack", throwIfNotFound: true);
         m_Player_Interact = m_Player.FindAction("Interact", throwIfNotFound: true);
         m_Player_Crouch = m_Player.FindAction("Crouch", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
@@ -1248,6 +1269,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Look;
     private readonly InputAction m_Player_Attack;
+    private readonly InputAction m_Player_SecondAttack;
     private readonly InputAction m_Player_Interact;
     private readonly InputAction m_Player_Crouch;
     private readonly InputAction m_Player_Jump;
@@ -1278,6 +1300,10 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Player/Attack".
         /// </summary>
         public InputAction @Attack => m_Wrapper.m_Player_Attack;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/SecondAttack".
+        /// </summary>
+        public InputAction @SecondAttack => m_Wrapper.m_Player_SecondAttack;
         /// <summary>
         /// Provides access to the underlying input action "Player/Interact".
         /// </summary>
@@ -1341,6 +1367,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @Attack.started += instance.OnAttack;
             @Attack.performed += instance.OnAttack;
             @Attack.canceled += instance.OnAttack;
+            @SecondAttack.started += instance.OnSecondAttack;
+            @SecondAttack.performed += instance.OnSecondAttack;
+            @SecondAttack.canceled += instance.OnSecondAttack;
             @Interact.started += instance.OnInteract;
             @Interact.performed += instance.OnInteract;
             @Interact.canceled += instance.OnInteract;
@@ -1382,6 +1411,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @Attack.started -= instance.OnAttack;
             @Attack.performed -= instance.OnAttack;
             @Attack.canceled -= instance.OnAttack;
+            @SecondAttack.started -= instance.OnSecondAttack;
+            @SecondAttack.performed -= instance.OnSecondAttack;
+            @SecondAttack.canceled -= instance.OnSecondAttack;
             @Interact.started -= instance.OnInteract;
             @Interact.performed -= instance.OnInteract;
             @Interact.canceled -= instance.OnInteract;
@@ -1685,6 +1717,13 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnAttack(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "SecondAttack" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnSecondAttack(InputAction.CallbackContext context);
         /// <summary>
         /// Method invoked when associated input action "Interact" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
         /// </summary>
--- Assets/+++Workdata/Scenes/Player/PlayerController.cs
+++ Assets/+++Workdata/Scenes/Player/PlayerController.cs
@@ -8,16 +8,23 @@ public class PlayerController : MonoBehaviour
     public static readonly int Hash_MovementValue = Animator.StringToHash("MovementValue");
     public static readonly int Hash_GroundValue = Animator.StringToHash("isGrounded");
     public static readonly int Hash_JumpValue = Animator.StringToHash("isJumping");
-
+    private static readonly int Hash_Actionid = Animator.StringToHash("ActionId");
+    private static readonly int Hash_ActionTrigger = Animator.StringToHash("ActionTrigger");
+    private static readonly int Hash_HoldingMouse = Animator.StringToHash("HoldingMouse");
+    
     #region Insepctor Variables
 
 // Alle Variabeln die im Inspektor zu sehen sind. 
     [SerializeField] private float _walkingSpeed = 5f;
     [SerializeField] private float _runningSpeed = 7f;
+    public bool _isSprinting;
+    public float _sprintSpeed;
     [SerializeField] private float _jumpForce = 7f;
     [SerializeField] private Transform _groundCheck;
     [SerializeField] private float _groundCheckRadius = 0.2f;
     [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private Vector2 boxOffset;
+    private Vector2 boxsize;
 
     #endregion
 
@@ -30,7 +37,8 @@ public class PlayerController : MonoBehaviour
     private float _moveSpeed = 5f;
 
     private bool _isGrounded;
-
+    private bool _isHoldingMouse;
+    
     //private bool isFacingRight = true;
     private Animator _anim;
 
@@ -47,6 +55,7 @@ public class PlayerController : MonoBehaviour
     private InputAction _moveAction;
     private InputAction _jumpAction;
     private InputAction _attackAction;
+    private InputAction _secondAttackAction;
     private InputAction _crouchAction;
     private InputAction _sprintAction;
 
@@ -74,6 +83,7 @@ public class PlayerController : MonoBehaviour
         _moveAction = _inputActions.Player.Move;
         _jumpAction = _inputActions.Player.Jump;
         _attackAction = _inputActions.Player.Attack;
+        _secondAttackAction = _inputActions.Player.SecondAttack;
         _crouchAction = _inputActions.Player.Crouch;
         _sprintAction = _inputActions.Player.Sprint;
 
@@ -87,7 +97,12 @@ public class PlayerController : MonoBehaviour
         _moveAction.performed += Move;
         _moveAction.canceled += Move;
         _jumpAction.performed += Jump;
-        _attackAction.performed += Attack;
+        _sprintAction.performed += Sprint;
+        _sprintAction.canceled += Sprint;
+        _attackAction.started += Attack;
+        _attackAction.canceled += AttackReleased;
+        _secondAttackAction.started += SecondAttack;
+        
 
     }
 
@@ -120,6 +135,7 @@ public class PlayerController : MonoBehaviour
         _moveAction.canceled -= Move;
         _jumpAction.performed -= Jump;
         _attackAction.performed -= Attack;
+        _secondAttackAction.canceled -= SecondAttack;
 
     }
 
@@ -146,23 +162,60 @@ public class PlayerController : MonoBehaviour
     {
         if (_isGrounded)
         {
-            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _jumpForce);
+            //_rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _jumpForce);
+            _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            _anim.SetTrigger(Hash_JumpValue);
         }
     }
 
+    private void Sprint(InputAction.CallbackContext ctx)
+    {
+        _isSprinting = !_isSprinting;
+        _moveSpeed = _isSprinting ? _sprintSpeed : _walkingSpeed;
+
+    }
+    
     void Attack(InputAction.CallbackContext ctx)
     {
         Debug.Log("mouse clicked");
         _anim.SetInteger("ActionID",10);
         _anim.SetTrigger("ActionTrigger" );
+        _isHoldingMouse = true;
+        _anim.SetBool(Hash_HoldingMouse,true);
     }
 
+    void AttackReleased(InputAction.CallbackContext ctx)
+    {
+        _anim.SetBool(Hash_HoldingMouse, false);
+    }
+
+    
+    private void SecondAttack(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Mouse clicked");
+        _anim.SetTrigger(Hash_ActionTrigger);
+        _anim.SetInteger("ActionID", 11);
+        
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = _isGrounded ? Color.green : Color.red;
+        Gizmos.DrawWireCube(boxOffset + (Vector2)transform.position, boxsize);
+    }
     #endregion
 
 
 
         #region Animation Methods
 
+        private void AnimationSetActionId (int id)
+        {
+  
+            _anim.SetInteger(Hash_Actionid, id);
+            _anim.SetTrigger(Hash_ActionTrigger);
+            _anim.SetBool(Hash_HoldingMouse, true);
+        }
         void UpdateAnimator()
         {
             _anim.SetFloat(Hash_MovementValue, Mathf.Abs(_rb.linearVelocity.x));
