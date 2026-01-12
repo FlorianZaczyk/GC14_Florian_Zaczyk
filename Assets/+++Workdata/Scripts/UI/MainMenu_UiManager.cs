99350d5924d3e67879b1b18474ba9a972baa2a0d
FlorianZaczyk <zaczyk.florian@gmail.com>
2026-01-12 13:36:06 +0100

    12.01.2026 UI + Scenes + Prefabs
    UI Canvas hinzugefügt mit  MainMenu, LoadMenu, OptionsMenu
    und dazu buttons hinzugefügt, die mit scripten hinzugefügt wurden.
    
    Neue Scenes
    
    Prefabs von Button

---
+++ Assets/+++Workdata/Scripts/UI/MainMenu_UiManager.cs
@@ -0,0 +1,40 @@
+
+using System;
+using UnityEngine;
+
+public class MainMenu_UiManager : MonoBehaviour
+{
+    public GameObject mainMenuContainer;
+    public GameObject loadMenuContainer;
+    public GameObject optionsMenuContainer;
+    
+    public GameObject _currentMenu;
+
+    private void Awake()
+    {
+        _currentMenu = mainMenuContainer;
+    }
+
+    public void OpenLoadMenu()
+    {
+        _currentMenu.SetActive(false);
+        
+        loadMenuContainer.SetActive(true);
+        _currentMenu = loadMenuContainer;
+    }
+
+    public void OpenMainMenu()
+    {
+        _currentMenu.SetActive(false);
+        
+        mainMenuContainer.SetActive(true);
+        _currentMenu = mainMenuContainer;
+    }
+    
+    public void OpenOptionsMenu()
+    {
+        _currentMenu.SetActive(false);
+        optionsMenuContainer.SetActive(true);
+        _currentMenu = optionsMenuContainer;
+    }
+}
