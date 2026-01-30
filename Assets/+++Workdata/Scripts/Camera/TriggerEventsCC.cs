434d1466ff4a14b5af8c93a47141d92c7710f932
Hafermilch361 <zaczyk.florian@gmail.com>
2026-01-30 20:55:34 +0100

    Camera fixed+ Lvl Tilemap
    -Camera + camera events fixeed mit skript trigger
    -Level weiter design mit Tilesmap

---
+++ Assets/+++Workdata/Scripts/Camera/TriggerEventsCC.cs
@@ -0,0 +1,40 @@
+using System;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventsCC : MonoBehaviour
+{
+    public UnityEvent onTriggerEnter;
+    public UnityEvent onTriggerExit;
+
+    private CinemachineCamera cm;
+    public AudioSource _audio;
+
+    public void Start()
+    {
+        cm = gameObject.GetComponent<CinemachineCamera>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cm.Priority.Value = 5;
+            _audio.Play();
+        }
+        
+        
+        
+    }
+    
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cm.Priority.Value = 0;
+            _audio.Stop();
+        }
+    
+    }
+}
