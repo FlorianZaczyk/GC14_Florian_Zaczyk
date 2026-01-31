cc850cab6bc1a165a755f68b7d5d3fee2ef96d36
Hafermilch361 <zaczyk.florian@gmail.com>
2026-01-31 14:36:46 +0100

    Elevator
    -Elevator finished
    -Lvl Design angepasst
    -Kopie von Platformer tile set gemacht + scales geändert für elevator

--- Assets/+++Workdata/Scripts/Interactable/ElevatorBehaviour.cs
+++ Assets/+++Workdata/Scripts/Interactable/ElevatorBehaviour.cs
@@ -5,23 +5,21 @@ public class ElevatorBehaviour : MonoBehaviour
     public static int HashUp = Animator.StringToHash("Up");
     public static int HashCall = Animator.StringToHash("Call");
 
-    
-    [SerializeField] private Animator anim;
-
+    [SerializeField] private Animator _anim;
     [SerializeField] private bool _elevatorIsUp;
 
     public void CallElevator(bool isUp)
     {
         if ((_elevatorIsUp && isUp) || (!_elevatorIsUp && !isUp)) return;
         
-        anim.SetBool(HashUp, isUp);
-        anim.SetTrigger(HashCall);
+        _anim.SetBool(HashUp, isUp);
+        _anim.SetTrigger(HashCall);
     }
 
     public void SwitchDirection()
     {
         _elevatorIsUp = !_elevatorIsUp;
-        anim.SetBool(HashUp, _elevatorIsUp);
-        anim.SetTrigger(HashCall);
+        _anim.SetBool(HashUp, _elevatorIsUp);
+        _anim.SetTrigger(HashCall);
     }
 }
